ca2b06f236ddfd2e7429083d54d41cfa613e74df
Illia Oliynik <illiaoliynik@gmail.com>
2020-04-23 21:13:02 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2864373d6bc61c7e48f9e43b143db318.js"
+  "./precache-manifest.0236d9d7a3970883d0b12abad9aa9402.js"
 );
 
 self.addEventListener('message', (event) => {
